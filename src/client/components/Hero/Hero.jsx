7b7e22cee86d573d36e23c60168ef222fb301e50
m0n02hz <mohit.Nagar@walmartlabs.com>
2020-08-12 01:06:24 +0530

    LP Bug fix for - register button not working

--- src/client/components/Hero/Hero.jsx
+++ src/client/components/Hero/Hero.jsx
@@ -10,7 +10,7 @@ const Hero = props => {
       <div className="row">
         <div className="col c-Hero__tag-line-container">
           <div className="c-Hero__tag-line">{CONSTANTS.LOGIN.LANDING_PAGE_TEXT}</div>
-          <div className="c-Hero__register-btn btn btn-primary no-border-radius" href={props.registerRedirectLink}>{CONSTANTS.LOGIN.REGISTER_TEXT}</div>
+          <a className="c-Hero__register-btn btn btn-primary no-border-radius" href={props.registerRedirectLink}>{CONSTANTS.LOGIN.REGISTER_TEXT}</a>
         </div>
         <div className="col">
           <img className="c-Hero__introImage" src={images[CONSTANTS.LOGIN.IMAGE_WALMART_INTRO]} />
