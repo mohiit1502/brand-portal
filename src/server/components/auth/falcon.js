1f16fe931eb1eca8928ee70eb5975bed4ebecb17
Mohit Nagar <mohit.Nagar@walmartlabs.com>
2020-09-11 12:26:17 +0530

    Created new version for stg build

--- src/server/components/auth/falcon.js
+++ src/server/components/auth/falcon.js
@@ -1,8 +1,8 @@
-import CONSTANTS, {CONSTANTS as VARS} from "../../constants/server-constants";
+import {CONSTANTS} from "../../constants/server-constants";
 import ServerUtils from "../../utility/server-utils";
 import queryString from "query-string";
 
-const secrets = require(VARS.PATH);
+const secrets = require(CONSTANTS.PATH);
 
 class Falcon {
 
@@ -15,10 +15,8 @@ class Falcon {
       const state = ServerUtils.randomStringGenerator(secrets.NONCE_STRING_LENGTH).toUpperCase();
       const scope = secrets.SCOPE;
       const responseType = secrets.RESPONSE_TYPE;
-      // const isInternal = secrets.IS_INTERNAL;
       const baseUrl = action.toLowerCase() === "login" ? IAM.FALCON_LOGIN_URL : IAM.FALCON_REGISTER_URL;
-      // const redirectUri = CONSTANTS.IAM.BASE_URL + IAM.REDIRECT_PATH;
-      const redirectUri = IAM.BASE_URL + IAM.REDIRECT_PATH;
+      const redirectUri = `${process.env.NODE_ENV === "development" ? CONSTANTS.BASE_URL : IAM.BASE_URL}${IAM.REDIRECT_PATH}`;
 
       return queryString.stringifyUrl({
         url: baseUrl,
