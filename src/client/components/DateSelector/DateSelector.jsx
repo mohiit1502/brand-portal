1b33e11029757c5ed51fb734356fa565ce182e2a
Devanshi Choudhary - d0c09oc <Devanshi.Choudhary@walmartlabs.com>
2020-11-29 18:07:28 +0530

    Update dateselector

--- src/client/components/DateSelector/DateSelector.jsx
+++ src/client/components/DateSelector/DateSelector.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import PropTypes from "prop-types";
 import "./DateSelector.component.scss";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 
 const DateSelector = props => {
 
@@ -12,12 +14,75 @@ const DateSelector = props => {
 
   }
 
+  const [inStartDate, setinStartDate] = useState(new Date());
+  const [inEndDate, setinEndDate] = useState(new Date());
+  const [startDate, setStartDate] = useState();
+  const [endDate, setEndDate] = useState();
+
+  function handleStartDate(date) {
+    console.log(date);
+    const a = date != null ? moment(date).format("yyyy-MM-DD") : subDays(new Date(), 15);
+    setStartDate(a);
+
+}
+function handleEndDate(date) {
+    console.log(date)
+    const a = date != null ? moment(date).format("yyyy-MM-DD") : subDays(new Date(), 0);
+    setEndDate(a);
+}
+
+
   return (
     <div className="c-DateSelector modal fade show" id="singletonModal" tabIndex="-1" role="dialog">
       <div className="modal-dialog modal-dialog-centered" role="document">
         <div className="modal-content">
           <div className="modal-header align-items-center">
             Custom Date Range Trial
+            <Form>
+                <Form.Group widths='equal'>
+                                        <Form.Field>
+                                            {/* <label>Start Date</label> */}
+                                            <DatePicker
+                                                selected={inStartDate}
+                                                // onChange={date => setStartDate(date)}
+                                                onChange={(date) => { handleStartDate(date); setinStartDate(date) }}
+                                                selectsStart
+                                                // startDate={startDate}
+                                                // endDate={endDate}
+                                                className="customDropdown"
+                                                dateFormat="yyyy/MM/dd"
+                                                fixedHeight
+                                                minDate={subDays(new Date(), 15)}
+                                                maxDate={subDays(new Date(), 0)}
+                                                showDisabledMonthNavigation
+                                                // dateFormat="yyyy h:mm aa"
+                                                // showTimeInput
+                                                isClearable
+
+                                            />
+                                        </Form.Field>
+                                        <Form.Field>
+                                            {/* <label>End Date</label> */}
+                                            <DatePicker
+                                                selected={inEndDate}
+                                                onChange={date => { handleEndDate(date); setinEndDate(date) }}
+                                                selectsEnd
+                                                // startDate={startDate}
+                                                // endDate={endDate}
+                                                // minDate={startDate}
+                                                className="customDropdown"
+                                                dateFormat="yyyy/MM/dd"
+                                                fixedHeight
+                                                minDate={subDays(new Date(), 15)}
+                                                maxDate={subDays(new Date(), 0)}
+                                                // showTimeInput
+                                                showDisabledMonthNavigation
+                                                // disabled
+                                                isClearable
+                                            />
+                                        </Form.Field>
+                                    </Form.Group>
+              </Form>
             <button type="button" className="close text-white" aria-label="Close" onClick={resetTemplateStatus}>
               <span aria-hidden="true">&times;</span>
             </button>
