e328b4d6618115f7b9e2dad6284bf4a0bf11c9e0
m0p028c <m.prasanth@walmartlabs.com>
2020-11-30 11:13:09 +0530

    Date Selector Change

--- src/client/components/DateSelector/DateSelector.jsx
+++ src/client/components/DateSelector/DateSelector.jsx
@@ -1,117 +1,123 @@
 import React from "react";
 import PropTypes from "prop-types";
 import DatePicker from "react-datepicker";
+// import moment from "moment";
 import "react-datepicker/dist/react-datepicker.css";
 
-  class DateSelector extends React.Component {
-    constructor () {
-      super();
-      this.state = {
-       // startDate: moment().add(-7, 'days'),
-        //endDate: moment()
-      }
-      this.handleStartDateChanged = this.handleStartDateChanged.bind(this);
-      this.handleEndDateChanged = this.handleEndDateChanged.bind(this);
-      this.getStats = this.getStats.bind(this);
-      this.getStatsFromServer = this.getStatsFromServer.bind(this);
+class DateSelector extends React.Component {
+  constructor () {
+    super();
+    this.state = {
+      startDate: Date.now(),
+      endDate: Date.now()
+      //  startDate: moment().add(-7, 'days'),
+      //   endDate: moment()
     }
+    this.handleStartDateChanged = this.handleStartDateChanged.bind(this);
+    this.handleEndDateChanged = this.handleEndDateChanged.bind(this);
+    this.getStats = this.getStats.bind(this);
+    this.getStatsFromServer = this.getStatsFromServer.bind(this);
+  }
 
-    componentDidMount(){
-      const self = this;
-      self.getStatsFromServer();
-    }
+  componentDidMount(){
+    const self = this;
+    self.getStatsFromServer();
+  }
 
-    getStatsFromServer()
+  getStatsFromServer()
+  {
+    console.log("This is get stats")
+
+    const startDate = this.state.startDate;
+    const endDate = this.state.endDate;
+    console.log("Get Stats for dates" + startDate + " and " + endDate );
+
+  }
+
+  getStats(e){
+    e.preventDefault();
+    this.getStatsFromServer();
+  }
+
+  handleStartDateChanged(date) {
+    let startDate = date;
+    let endDate = this.state.endDate;
+    if (startDate >= endDate)
     {
-      const startDate = this.state.startDate.format('L');
-      const endDate = this.state.endDate.format('L');
-      console.log("Get Stats for dates" + startDate + " and " + endDate );
-
+      console.log ('startDate more than end date');
+      //  endDate = moment(date).add(1, 'days');
     }
-
-    getStats(e){
-        e.preventDefault();
-        this.getStatsFromServer();
+    this.setState({
+      startDate: startDate,
+      endDate: endDate
+    });
+  }
+  handleEndDateChanged(date) {
+    let startDate = this.state.startDate;
+    let endDate = date;
+    if (startDate >= endDate)
+    {
+      console.log ('startDate more than end date');
+      // startDate = moment(date).add(-1, 'days');
     }
+    this.setState({
+      startDate: startDate,
+      endDate: endDate
+    });
+  }
 
-    handleStartDateChanged(date) {
-        let startDate = date;
-        let endDate = this.state.endDate;
-        if (startDate >= endDate)
-        {
-            console.log ('startDate more than end date');
-           // endDate = moment(date).add(1, 'days');
-        }
-        this.setState({
-          startDate: startDate,
-          endDate: endDate
-        });
-    }
-    handleEndDateChanged(date) {
-          let startDate = this.state.startDate;
-          let endDate = date;
-          if (startDate >= endDate)
-          {
-            console.log ('startDate more than end date');
-            //startDate = moment(date).add(-1, 'days');
-          }
-          this.setState({
-            startDate: startDate,
-            endDate: endDate
-          });   
-        }
-    
-    render () {
+  render () {
 
 
-      const statsTableStyle =
+    const statsTableStyle =
       {
         "margin": "10px"
       }
 
-      const numberColStyle = 
+    const numberColStyle =
       {
         "padding-left": "10px"
       }
 
-      const resetTemplateStatus = () => {
+    const resetTemplateStatus = () => {
 
-      }
-    
-      const handleSubmit = () => {
-    
-      }
+    }
 
-  return (
-    <div className="c-DateSelector modal fade show" id="singletonModal" tabIndex="-1" role="dialog">
-      <div className="modal-dialog modal-dialog-centered" role="document">
-        <div className="modal-content">
-          <div className="modal-header align-items-center">
-            Custom Date Range
-            <button type="button" className="close text-white" aria-label="Close" onClick={resetTemplateStatus}>
-              <span aria-hidden="true">&times;</span>
-            </button>
-          </div>
-          <div className="modal-body text-center p-5">
+    const handleSubmit = () => {
+
+    }
+
+    return (
+      <div className="c-DateSelector modal fade show" id="singletonModal" tabIndex="-1" role="dialog">
+        <div className="modal-dialog modal-dialog-centered" role="document">
+          <div className="modal-content">
+            <div className="modal-header align-items-center">
+              Custom Date Range
+              <button type="button" className="close text-white" aria-label="Close" onClick={resetTemplateStatus}>
+                <span aria-hidden="true">&times;</span>
+              </button>
+            </div>
+            <div className="modal-body text-center p-5">
               <span>Start Date:</span>
-              <span>Start Date:</span>
+              {/* <span>Start Date:</span> */}
               <DatePicker selected={this.state.startDate}
-               onChange={this.handleStartDateChanged}
-               /> 
+                          onChange={this.handleStartDateChanged}
+              />
               <span style={{"paddingLeft": "20px"}}>End Date:</span>
               <DatePicker
-                  selected={this.state.endDate}
-                  onChange={this.handleEndDateChanged}
-                />
+                selected={this.state.endDate}
+                onChange={this.handleEndDateChanged}
+              />
+              <button style={{"marginLeft": "20px"}} onClick={(e) => {this.getStats(e)}}>Get Stats</button>
               <form onSubmit={handleSubmit} className="h-100 px-2">
               </form>
             </div>
 
+          </div>
         </div>
       </div>
-    </div>
-  );
-}
+    );
+  }
 }
 
 DateSelector.propTypes = {
