ccf42f506a706b2ccf965d592d512d7a2f6da2be
Mohit Nagar <mohiit1502@gmail.com>
2021-02-12 15:19:31 +0530

    Added loggers in server http

--- src/server/utility/ServerHttp.js
+++ src/server/utility/ServerHttp.js
@@ -1,121 +1,71 @@
 import fetch from "node-fetch";
 import queryString from "query-string";
 import ServerHttpError from "./ServerHttpError";
+import CONSTANTS from "../../client/constants/constants";
+import ClientHttpError from "../../client/utility/ClientHttpError";
 
 export default class ServerHttp {
 
   static async get(url, options, queryParams) {
-    try{
-      !url && console.log("No URL!!");
-      const urlString = queryString.stringifyUrl({url, query: queryParams});
-
-      const res = await fetch(urlString, {
-        method: "GET",
-        ...options
-      });
-
-      const {ok, status, headers} = res;
-
-      if (ok) {
-        if (headers.get("content-type") === "application/json") {
-          return {status, body: await res.json()};
-        }
-        return res;
-      }
-      const err = await res.json();
-      console.error("Error from ServerHttp.get: ", err);
-      throw new ServerHttpError(status, err.error, err.message, err.code);
-    } catch (err) {
-      console.error("Caught in ServerHttp.get: ", err);
-      return err;
-    }
+    const urlString = queryString.stringifyUrl({url, query: queryParams});
+    options.method = "GET";
+    return await ServerHttp.crud(urlString, options, "get");
   }
 
   static async post(url, options, data) {
-
-    options.headers = options.headers || {};
-    options.headers = { "Content-Type": "application/json", ...options.headers };
-    const response = await fetch(url, {
-      method: "POST",
-      body: typeof data === "string" ? data : JSON.stringify(data),
-      ...options
-    });
-
-    const {ok, status, headers} = response;
-
-    if (ok) {
-      if (headers.get("content-type") === "application/json") {
-        return {status, body: await response.json()};
-      }
-      return response;
-    }
-
-    const err = await response.json();
-    console.log(err);
-    throw new ServerHttpError(status, err.error, err.message);
+    options.headers = options.headers ? {"Content-Type": "application/json", ...options.headers} : {"Content-Type": "application/json"};
+    options.method = "POST";
+    options.body = typeof data === "string" ? data : JSON.stringify(data);
+    return await ServerHttp.crud(url, options, "post");
   }
 
   static async postAsFormData(url, options, data) {
-    const response = await fetch(url, {
-      method: "POST",
-      body: data,
-      ...options
-    });
-    const {ok, status, headers} = response;
-    if (ok) {
-      if (headers.get("content-type") === "application/json") {
-        const responseBody = await response.json();
-        return {status, body: responseBody};
-      }
-      return response;
-    }
-
-    const err = await response.json();
-    console.log("2. In ServerHttp - Capturing error for not Ok response ====== ", err);
-    throw new ServerHttpError(status, err.error, err.message);
+    options.method = "POST";
+    options.body = data;
+    return await ServerHttp.crud(url, options, "postAsFormData");
   }
 
   static async put(url, options, data) {
-
-    options.headers = options.headers || {};
-    options.headers = { "Content-Type": "application/json", ...options.headers };
-    const response = await fetch(url, {
-      method: "PUT",
-      body: JSON.stringify(data),
-      ...options
-    });
-
-    const {ok, status, headers} = response;
-
-    if (ok) {
-      if (headers.get("content-type") === "application/json") {
-        const body = await response.json();
-        return {status, body};
-      }
-      return response;
-    }
-
-    const err = await response.json();
-    console.log(err);
-    throw new ServerHttpError(status, err.error, err.message);
+    options.headers = options.headers ? {"Content-Type": "application/json", ...options.headers} : {"Content-Type": "application/json"};
+    options.method = "PUT";
+    options.body = JSON.stringify(data);
+    return await ServerHttp.crud(url, options, "put");
   }
 
   static async delete(url, options, queryParams) {
     const urlString = queryString.stringifyUrl({url, query: queryParams});
+    options.method = "DELETE";
+    return await ServerHttp.crud(urlString, options, "delete");
+  }
 
-    const res = await fetch(urlString, {
-      method: "DELETE",
-      ...options
-    });
-
-    const {ok, status} = res;
-
-    if (ok) {
-      return res;
+  static async crud (urlString, options, method) {
+    try {
+      !urlString && console.log("No URL!!");
+      console.log("1. ===== Crud Request Start. Requesting URL: ", urlString)
+      const response = await fetch(urlString, options);
+      const {ok, status, headers} = response;
+      if (ok) {
+        console.log("2. Response is OK with status: ", status);
+        if (headers.get("content-type") === "application/json") {
+          return {status, body: await response.json()};
+        }
+        return {status, body: await response.text()};
+      }
+      console.log("3. Response not OK, logging response: ", response);
+      let err;
+      try {
+        err = await response.json();
+      } catch (e) {
+        console.log("4. Couldn't parse response as JSON, trying as text...");
+        err = await response.text();
+      }
+      console.log(`5. In ServerHttp.${method} - Capturing error for not Ok response ====== `, err);
+      throw new ServerHttpError(status, err.error, err.message);
+    } catch (e) {
+      console.error(`6. Caught in ServerHttp.${method}: `, err);
+      throw new ServerHttpError(500, e);
+    } finally {
+      console.log("7. === Crud Request End!");
     }
-
-    const err = await res.json();
-    console.log(err);
-    throw new ServerHttpError(status, err.error, err.message);
   }
 }
