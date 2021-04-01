f194357663d41919607ec8d74973d3a42c0d17a8
m0n02hz <mohit.nagar@walmart.com>
2021-04-02 02:02:02 +0530

    Added authorizations for new roles "Pending Supplier Activation" and "TouNotAccepted", added back kraken workflows posting to create user API, fixed styling for larger column requirement for "Pending Supplier Activation"

--- src/client/components/custom-components/modal/templates/create-user-template.jsx
+++ src/client/components/custom-components/modal/templates/create-user-template.jsx
@@ -251,7 +251,7 @@ class CreateUserTemplate extends React.Component {
         phoneNumber: this.state.form.inputData.phone.value,
         type: isThirdParty ? CONSTANTS.USER.USER_TYPE.THIRD_PARTY : CONSTANTS.USER.USER_TYPE.INTERNAL
       },
-      // krakenUniqueWorkflow: this.state.uniquenessCheckStatus
+      krakenUniqueWorkflow: this.state.uniquenessCheckStatus
     };
 
     isThirdParty && (payload.user.companyName = this.state.form.inputData.companyName.value);
