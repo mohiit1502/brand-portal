2da4e485aa40c856ee65623a1481886ea0b6a657
m0n02hz <mohit.nagar@walmart.com>
2021-05-17 01:26:05 +0530

    Committing pending webform component

---
+++ src/client/components/WebForm/WebForm.test.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import WebForm from './WebForm';
+
+describe('WebForm', () => {
+   it('renders without error', () => {
+
+   });
+});
---
+++ src/client/components/WebForm/index.js
@@ -0,0 +1,3 @@
+import WebForm from './WebForm.jsx';
+
+export default WebForm;
