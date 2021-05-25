1ca930b348ffc5ee6c4c09f8cb99fd8c37f37580
deekshith <deekshith.vs@walmartlabs.com>
2021-05-25 15:30:02 +0530

    adding workflow to view user profile event

--- src/client/components/custom-components/headers/home-header.jsx
+++ src/client/components/custom-components/headers/home-header.jsx
@@ -48,7 +48,7 @@ class HomeHeader extends React.Component {
               </Link>
               <div className="dropdown-menu dropdown-menu-right no-border-radius shadow-sm mt-2">
                 {
-                  this.props.isOnboarded && <a className="dropdown-item" href={CONSTANTS.ROUTES.PROFILE.USER} onClick={ () => {mixpanel.trackEvent(MIXPANEL_CONSTANTS.USER_PROFILE.VIEW_USER_PROFILE);}}>Profile</a>
+                  this.props.isOnboarded && <a className="dropdown-item" href={CONSTANTS.ROUTES.PROFILE.USER} onClick={ () => {mixpanel.trackEvent(MIXPANEL_CONSTANTS.USER_PROFILE.VIEW_USER_PROFILE, mixpanelPayload);}}>Profile</a>
                 }
                 <a className="dropdown-item" href={logoutUrl} onClick={() => {mixpanel.logout(MIXPANEL_CONSTANTS.LOGOUT.LOGOUT, mixpanelPayload);}}>Logout</a>
               </div>
