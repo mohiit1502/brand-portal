d241a0779da0bcdc28d9f63efe74b6797c3e1081
m0n02hz <mohit.nagar@walmart.com>
2021-09-21 08:00:35 +0530

    Added loader config to karma

--- archetype/config/karma/karma.conf.coverage.js
+++ archetype/config/karma/karma.conf.coverage.js
@@ -7,6 +7,17 @@ module.exports = function (config, settings) {
           base: "ChromeHeadless",
           flags: ["--no-sandbox"]
         }
+      },
+      webpack: { //kind of a copy of your webpack config
+        devtool: 'inline-source-map', //just do inline source maps instead of the default
+        module: {
+          loaders: [
+            { test: /\.(pdf)$/, loader: 'file-loader' }
+          ]
+        }
+      },
+      webpackServer: {
+        noInfo: true //please don't spam the console when running in karma!
       }
     })
   );
