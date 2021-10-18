9f1692b018b25b4135a05d7ff6f20f1060d28a6d
m0p028c <m.prasanth@walmartlabs.com>
2021-10-18 19:57:52 +0530

    commenting karma config

--- archetype/config/karma/karma.conf.coverage.js
+++ archetype/config/karma/karma.conf.coverage.js
@@ -1,13 +1,13 @@
-module.exports = function (config, settings) {
-  config.set(
-    Object.assign({}, settings, {
-      browsers: ["ChromeHeadlessNoSandbox"],
-      customLaunchers: {
-        ChromeHeadlessNoSandbox: {
-          base: "ChromeHeadless",
-          flags: ["--no-sandbox"]
-        }
-      }
-    })
-  );
-};
+// module.exports = function (config, settings) {
+//   config.set(
+//     Object.assign({}, settings, {
+//       browsers: ["ChromeHeadlessNoSandbox"],
+//       customLaunchers: {
+//         ChromeHeadlessNoSandbox: {
+//           base: "ChromeHeadless",
+//           flags: ["--no-sandbox"]
+//         }
+//       }
+//     })
+//   );
+// };
