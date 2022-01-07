8a113e29bff393782d605f6a9fae14057aa6f2c1
m0n02hz <mohit.nagar@walmart.com>
2022-01-07 09:19:06 +0530

    Added babel to enable usage of finally

--- test/client/utility/validationUtil.test.js
+++ test/client/utility/validationUtil.test.js
@@ -1,4 +1,5 @@
-import '@babel/polyfill';
+import "core-js/stable";
+import "regenerator-runtime/runtime";
 import fetch from "node-fetch";
 import Validator from "../../../src/client/utility/validationUtil";
 import Http from "../../../src/client/utility/Http";
