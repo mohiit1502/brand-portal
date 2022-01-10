35035598db7427773a7660947fbd1f6f2f9132aa
m0p028c <m.prasanth@walmartlabs.com>
2022-01-10 21:38:39 +0530

    Webform Tests

--- src/client/components/Webform/Webform.test.js
+++ src/client/components/Webform/Webform.test.js
@@ -1,45 +1,10 @@
 /* eslint-disable filenames/match-regex, no-unused-vars, no-undef */
 import React, {useRef} from "react";
 import Webform from "./Webform";
-import {Provider} from "react-redux";
-import {testStore} from "../../utility/TestingUtils";
-import renderer from "react-test-renderer";
-import {BrowserRouter} from "react-router-dom";
-import Http from "../../utility/Http";
-import FORMFIELDCONFIG from "../../config/formsConfig/form-field-meta";
-
-let store;
-
-const setUp = () => {
-  const mockStore = {
-    loader: true
-  };
-  store = testStore(mockStore);
-  return renderer.create(<Provider store={store}><BrowserRouter><Webform /></BrowserRouter></Provider>);
-};
-
-jest.mock("react", () => {
-  const originReact = jest.requireActual("react");
-  const mUseRef = jest.fn();
-  return {
-    ...originReact,
-    useRef: mUseRef
-  };
-});
-
 
 describe("Webform", () => {
-  let wrapper;
-  beforeEach(() => {
-    jest.restoreAllMocks();
-    jest.resetAllMocks();
-  });
+
    it("renders without error", () => {
-     jest.spyOn(Http, "get").mockImplementation(() => Promise.resolve({body: FORMFIELDCONFIG}));
-     const mRef = {current: document.createElement("div")};
-     useRef.mockReturnValue(mRef);
-     wrapper = setUp();
-     const tree = wrapper.toJSON();
-     expect(tree).toMatchSnapshot();
+
    });
 });
