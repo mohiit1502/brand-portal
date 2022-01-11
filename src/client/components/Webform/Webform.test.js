af5efb4d36c93097ecae78e98e2f1073d59ab429
m0n02hz <mohit.nagar@walmart.com>
2022-01-12 00:29:27 +0530

    Updated snapshots, added webform test

--- src/client/components/Webform/Webform.test.js
+++ src/client/components/Webform/Webform.test.js
@@ -1,31 +1,31 @@
 import React, {useRef} from "react";
 import Webform from "./Webform";
 import toJson from "enzyme-to-json";
-import {mount} from "enzyme";
+import {configure, mount} from "enzyme";
 import {Provider} from "react-redux";
 import MockNextContext from "../../../../test/client/utility/MockNextContext";
 import {clearKeys, testStore} from "../../utility/TestingUtils";
 import renderer from "react-test-renderer";
+import Adapter from "enzyme-adapter-react-16";
+import Http from "../../utility/Http";
 
+configure({adapter: new Adapter()});
 
 let props;
 let store;
 
 const setUp = (props) => {
-  store = testStore({});
+  store = testStore({modal: {}});
   return renderer.create(props ? <Provider store={store}><MockNextContext pathname="/help"><Webform {...props} /></MockNextContext></Provider> : <Provider store={store}><MockNextContext><Webform /></MockNextContext></Provider>);
 };
 
-jest.mock("react", () => {
-  const originReact = jest.requireActual("react");
-  const mUseRef = jest.fn();
-  return {
-    ...originReact,
-    useRef: mUseRef
+const setUpMounted = () => {
+  const mockStore = {
+    modal: {}
   };
-});
-
-
+  store = testStore(mockStore);
+  return mount(<Provider store={store}><Webform/></Provider>);
+};
 
 describe("Webform", () => {
   let wrapper;
@@ -47,6 +47,11 @@ describe("Webform", () => {
     const tree = wrapper.toJSON();
     clearKeys(tree, []);
     expect(tree).toMatchSnapshot();
-
+  });
+  it("renders the Webform without error", () => {
+    jest.spyOn(Http, "get").mockImplementation(() => Promise.resolve({body: {}}));
+    wrapper = setUpMounted();
+    toJson(wrapper);
+    // expect(tree).toMatchSnapshot();
   });
 });
