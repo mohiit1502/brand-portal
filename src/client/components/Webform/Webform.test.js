07f6d8cfa0e7fded9a6551cc7cd8591d1f0d2e85
a0s0ofg <Ayushi.Sharma@walmart.com>
2022-01-11 19:50:43 +0530

    unit tests-webform,widgetSectionContainer, WebformWorkFlowDecider

--- src/client/components/Webform/Webform.test.js
+++ src/client/components/Webform/Webform.test.js
@@ -1,10 +1,52 @@
-/* eslint-disable filenames/match-regex, no-unused-vars, no-undef */
 import React, {useRef} from "react";
 import Webform from "./Webform";
+import toJson from "enzyme-to-json";
+import {mount} from "enzyme";
+import {Provider} from "react-redux";
+import MockNextContext from "../../../../test/client/utility/MockNextContext";
+import {clearKeys, testStore} from "../../utility/TestingUtils";
+import renderer from "react-test-renderer";
+
+
+let props;
+let store;
+
+const setUp = (props) => {
+  store = testStore({});
+  return renderer.create(props ? <Provider store={store}><MockNextContext pathname="/help"><Webform {...props} /></MockNextContext></Provider> : <Provider store={store}><MockNextContext><Webform /></MockNextContext></Provider>);
+};
+
+jest.mock("react", () => {
+  const originReact = jest.requireActual("react");
+  const mUseRef = jest.fn();
+  return {
+    ...originReact,
+    useRef: mUseRef
+  };
+});
+
+
 
 describe("Webform", () => {
+  let wrapper;
+  beforeEach(() => {
+    jest.restoreAllMocks();
+    jest.resetAllMocks();
+  });
+  it("renders without error", () => {
+    props = {
+      configuration: {},
+      dispatchMetadata: ()=>{},
+      dispatchWebformState: ()=>{},
+      showNotification: ()=>{},
+      toggleModal: ()=>{},
+      webformFieldsConfiguration: {}
+    };
+    wrapper = setUp(props);
 
-   it("renders without error", () => {
+    const tree = wrapper.toJSON();
+    clearKeys(tree, []);
+    expect(tree).toMatchSnapshot();
 
-   });
+  });
 });
