5aa8b219886d621bae411e70c31a906325575236
m0n02hz <mohit.nagar@walmart.com>
2022-01-12 03:10:01 +0530

    updated lists snapshots

--- test/client/components/home/content-renderer/claim/claim-list.test.js
+++ test/client/components/home/content-renderer/claim/claim-list.test.js
@@ -10,26 +10,26 @@ import profile from "../../../../mocks/userProfile";
 import Http from "../../../../../../src/client/utility/Http";
 import currentFilters from "../../../../mocks/currentFilters";
 import Adapter from "enzyme-adapter-react-16";
+import MockNextContext from "../../../../utility/MockNextContext";
 
 configure({ adapter: new Adapter() });
 let store;
+const mockStore = {
+  dashboard: {
+    filter: currentFilters,
+    widgetAction: "test-widget-action"
+  },
+  claims: {
+    claimList: [],
+    fetchClaimsCompleted: true
+  },
+  modal: {},
+  user: {profile}
+};
 
-const setUp = (pathname) => {
-  const mockStore = {
-    dashboard: {
-      filter: currentFilters,
-      widgetAction: "test-widget-action"
-    },
-    claims: {
-      claimList: [],
-      fetchClaimsCompleted: true
-    },
-    modal: {},
-    user: {profile}
-  };
+const setUp = (pathname, mockStore) => {
   store = realStore(mockStore);
-
-  return mount(<Provider store={store}><ClaimList history={{location: {pathname}}} /></Provider>);
+  return mount(<Provider store={store}><MockNextContext><ClaimList history={{location: {pathname}}} /></MockNextContext></Provider>);
 };
 
 describe("ClaimList test container", () => {
@@ -42,26 +42,34 @@ describe("ClaimList test container", () => {
   describe("ClaimList renders without error", () => {
     it("should render the ClaimList successfully", () => {
       jest.spyOn(Http, "get").mockImplementation(() => Promise.resolve({body: {data: {content: [{firstName: "test", lastName: "test"}, {}]}}}));
-      wrapper = setUp("/claims");
+      wrapper = setUp("/claims", mockStore);
       let tree = toJson(wrapper);
       clearKeys(tree, []);
       expect(tree).toMatchSnapshot();
-      wrapper = setUp("/claims/")
+      wrapper = setUp("/claims/", mockStore)
       tree = toJson(wrapper);
       clearKeys(tree, []);
       expect(tree).toMatchSnapshot();
       wrapper.find(".table-row > .table-head-cell").at(1).simulate("click");
     });
+    it("should render without claim summary filter", () => {
+      let store = JSON.parse(JSON.stringify(mockStore));
+      delete store.dashboard.filter["widget-claim-summary"];
+      wrapper = setUp("/claims/", store)
+      const tree = toJson(wrapper);
+      clearKeys(tree, []);
+      expect(tree).toMatchSnapshot();
+    })
     it("displays claim details modal", () => {
       jest.spyOn(Http, "get").mockImplementation(() => Promise.resolve({body: {}}));
-      wrapper = setUp("/claims/BPCASE-123")
+      wrapper = setUp("/claims/BPCASE-123", mockStore)
       const tree = toJson(wrapper);
       clearKeys(tree, []);
       expect(tree).toMatchSnapshot();
     })
     it("tests for scenario when backend sends error", () => {
       jest.spyOn(Http, "get").mockImplementation(() => Promise.resolve({body: {errors: ["error"]}}));
-      wrapper = setUp();
+      wrapper = setUp("/claims", mockStore);
       const tree = toJson(wrapper);
       clearKeys(tree, []);
       expect(tree).toMatchSnapshot();
