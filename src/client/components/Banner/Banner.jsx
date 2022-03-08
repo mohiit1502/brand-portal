6203d2407a29a6d1bc0ac4194ebf36f7ee2fdb11
m0p028c <m.prasanth@walmartlabs.com>
2022-03-08 10:16:19 +0530

    Banner update

--- src/client/components/Banner/Banner.jsx
+++ src/client/components/Banner/Banner.jsx
@@ -15,8 +15,8 @@ const Banner = (props) => {
       <div className={`banner-body pl-4 pr-2 py-2`}>
         <div className={`banner-content pl-2 col-11`}>
           <strong>We do not support unauthorized reseller claims. </strong>
-          Walmart runs a unified catalog that allows resellers to make offers on a single listing.
-          {expand ? ` For unauthorized reseller claims, you may contact the seller directly to resolve the matter. The contact information can be accessed by clicking the specific seller’s name found on the listing.` : ""}
+          Walmart runs a unified catalog that allows resellers to make offers on a single listing.<br/>
+          {expand ? `For unauthorized reseller claims, you may contact the seller directly to resolve the matter. The contact information can be accessed by clicking the specific seller’s name found on the listing.` : ""}
         </div>
         <span className={`col-1 pl-3 pr-0`}><a className={`see-more-button`} onClick={func} href={`#`}>{expand ? `See less` : `See more`}</a></span>
       </div>
